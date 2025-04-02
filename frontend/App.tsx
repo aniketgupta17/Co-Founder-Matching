bb27d544f9a1401568b9e39136c14512f4dd8273
Saakshi Gupta <62831255+saakshigupta2002@users.noreply.github.com>
2025-04-02 18:41:37 +1000

    Update App.tsx

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,6 +1,14 @@
+import 'react-native-gesture-handler';
 import React from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 import RootNavigator from './app/navigation/RootNavigator';
 
 export default function App() {
-  return <RootNavigator />;
+  return (
+    <SafeAreaProvider>
+      <StatusBar style="auto" />
+      <RootNavigator />
+    </SafeAreaProvider>
+  );
 }
